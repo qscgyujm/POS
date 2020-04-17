c753666f3fbdb38c7854241a5dd9e2da5a0c5eaa
X <qscgyujm@gmail.com>
2020-04-17 11:39:07 +0800

    [FIX] http

--- src/api/apiRoot.js
+++ src/api/apiRoot.js
@@ -1,7 +1,7 @@
 /* eslint-disable import/prefer-default-export */
 import axios from 'axios';
 
-const url = 'http://localhost/api';
+const url = 'http://localhost:1337/api';
 
 export const apiRoot = axios.create({
   baseURL: url,
