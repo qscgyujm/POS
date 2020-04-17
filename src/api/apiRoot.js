14af81de469f670481a748e34f284b420d33cdc5
X <qscgyujm@gmail.com>
2020-04-17 11:05:07 +0800

    [FIX] change api path

--- src/api/apiRoot.js
+++ src/api/apiRoot.js
@@ -1,7 +1,7 @@
 /* eslint-disable import/prefer-default-export */
 import axios from 'axios';
 
-const url = 'http://localhost/api';
+const url = 'http://pos:1337';
 
 export const apiRoot = axios.create({
   baseURL: url,
